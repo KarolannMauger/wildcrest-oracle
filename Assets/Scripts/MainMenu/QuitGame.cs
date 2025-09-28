44bfe790a89846946025019e803be0aad16ebec4
KarolannMauger <karolannmauger@gmail.com>
2025-09-28 17:17:23 -0400

    feat: Add QuitGame script

---
+++ Assets/Scripts/MainMenu/QuitGame.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+ 
+[RequireComponent(typeof(Button))]
+public class QuitGame : MonoBehaviour
+{
+    private Button button;
+ 
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+ 
+    private void TaskOnClick()
+    {
+        Debug.Log("Quitter le jeu !");
+        Application.Quit();
+ 
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
